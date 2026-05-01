dee11f4de1b8024fba0ced408f986c938726ed89
Amit <amit@example.com>
2026-05-01 22:12:42 +0530

    Removed devops-lab-amit folder completely

--- devops-lab-amit/maven-app/src/main/java/com/amit/devops/App.java
+++
@@ -1,7 +0,0 @@
-package com.amit.devops;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Amit! Welcome to DevOps Automation with Maven!");
-    }
-}
